e231d724315c66dcd5efdd9ecc7c677ca8b4b12e
Arvin <lwz525@126.com>
2018-08-13 18:05:33 +0800

    .

---
+++ src/components/Message.vue
@@ -0,0 +1,45 @@
+<template>
+    <div v-show='show' :class="`alert alert-${type} alert-dismissible`">
+        <button class="close" type="button" @click="close"><span>x</span></button>
+        {{msg}}
+    </div>
+</template>
+<script>
+    export default{
+        name:'Message',
+        props:{
+            //是否显示消息框
+            show:{
+                type:Boolean,
+                default:false
+            },
+            type:{
+                type:String,
+                default:'success'
+            },
+            //消息
+            msg:{
+                type:String,
+                default:''
+            }
+        },
+
+        watch:{
+            show(value){
+                if(value){
+                    this.$nextTick(()=>{
+                        this.$el.scrollIntoView(true)
+                    })
+                }
+            }
+        },
+        methods:{
+            close(){
+                this.$emit('update:show',false)
+            }
+        }
+    }
+</script>
+<style scoped>
+
+</style>
